c0b69bcfff2015b11748308d14e84ee7adbd916d
Eleni Kelaiditi <elenkelaid@gmail.com>
2023-03-09 18:52:01 +0200

    Edge web driver

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+# driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver = webdriver.Edge(executable_path=EdgeChromiumDriverManager().install())
 
 driver.get('https://www.linkedin.com/')
 driver.maximize_window()
